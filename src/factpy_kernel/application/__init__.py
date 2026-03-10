7d21ca0d23b654b5fb119aa96eea6c7f9394b318
GigaGaiaWorld <aussehenferfekt@gmail.com>
2026-03-10 18:59:08 +0100

    Draft protocol DTO definitions

--- src/factpy_kernel/application/__init__.py
+++ src/factpy_kernel/application/__init__.py
@@ -1,2 +1,29 @@
 """Application-layer modules built on top of core runtime primitives."""
 
+from .schema_runtime import (
+    EntityTypeInfo,
+    IdentityFieldInfo,
+    PredicateInfo,
+    SchemaIndex,
+    SchemaResolutionError,
+    build_schema_index,
+    encode_entity_ref,
+    entity_info,
+    field_predicate,
+    materialize_identity,
+    resolve_selector,
+)
+
+__all__ = [
+    "EntityTypeInfo",
+    "IdentityFieldInfo",
+    "PredicateInfo",
+    "SchemaIndex",
+    "SchemaResolutionError",
+    "build_schema_index",
+    "encode_entity_ref",
+    "entity_info",
+    "field_predicate",
+    "materialize_identity",
+    "resolve_selector",
+]
